85bc4fef7e062e498b511a9e735a2b9dd813f104
Luisina Silvestre <luisina.silvestre0914@gmail.com>
2022-08-21 02:06:53 -0300

    entrega1

--- js/login.js
+++ js/login.js
@@ -4,10 +4,20 @@ btn.addEventListener('click', function(event){
     let email = document.getElementById('floatingInput');
     let pwrd = document.getElementById('floatingPassword');
 
-    if (email.value == 0 || pwrd.value == 0){
+    if (email.value == 0 && pwrd.value == 0){
         event.preventDefault();
         email.classList.add('is-invalid');
-        pwrd.classList.add('is-invalid')
+        pwrd.classList.add('is-invalid');   
+    }
+    else if (email.value != 0 && pwrd.value == 0){
+        event.preventDefault();
+        pwrd.classList.add('is-invalid');
+        email.classList.remove('is-invalid');
+    }
+    else if (email.value == 0 && pwrd.value != 0){
+        event.preventDefault();
+        email.classList.add('is-invalid');
+        pwrd.classList.remove('is-invalid');
     }
     else {
         sessionStorage.setItem('currentloggedin_email', email.value);
