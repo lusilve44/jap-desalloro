fde5f9d4c53860d52cd84fffeae96fc4109b0b94
Luisina Silvestre <luisina.silvestre0914@gmail.com>
2022-08-27 13:30:02 -0300

    avance 2 27-08

--- js/login.js
+++ js/login.js
@@ -3,25 +3,23 @@ let btn = document.getElementById('ingresar');
 btn.addEventListener('click', function(event){
     let email = document.getElementById('floatingInput');
     let pwrd = document.getElementById('floatingPassword');
+    event.preventDefault();
 
-    if (email.value == 0 && pwrd.value == 0){
-        event.preventDefault();
+    if (email.value.length == 0 && pwrd.value == ''){
         email.classList.add('is-invalid');
         pwrd.classList.add('is-invalid');   
     }
     else if (email.value != 0 && pwrd.value == 0){
-        event.preventDefault();
         pwrd.classList.add('is-invalid');
         email.classList.remove('is-invalid');
     }
     else if (email.value == 0 && pwrd.value != 0){
-        event.preventDefault();
         email.classList.add('is-invalid');
         pwrd.classList.remove('is-invalid');
     }
     else {
         sessionStorage.setItem('currentloggedin_email', email.value);
-        sessionStorage.setItem('status','loggedIn')
+        sessionStorage.setItem('status','loggedIn');
         location.href = "./index.html";
     }
 })
