4f2cab76e3d8483ad174ed2282ac4a723971a779
Luisina Silvestre <luisina.silvestre0914@gmail.com>
2022-09-04 22:38:52 -0300

    entrega 2 04-09

--- js/products.js
+++ js/products.js
@@ -11,13 +11,13 @@ user.innerHTML = `${sessionStorage.getItem('currentloggedin_email')}`;
 
 function sortProducts(criteria, array){
     let result = [];
-    if (criteria === ORDER_ASC_BY_PRICE){
+    if (criteria === ORDER_DESC_BY_PRICE){
         result = array.sort(function(a, b) {
             if ( a.cost < b.cost ){ return -1; }
             if ( a.cost > b.cost ){ return 1; }
             return 0;
         });
-    }else if (criteria === ORDER_DESC_BY_PRICE){
+    }else if (criteria === ORDER_ASC_BY_PRICE){
         result = array.sort(function(a, b) {
             if ( a.cost > b.cost ){ return -1; }
             if ( a.cost < b.cost ){ return 1; }
